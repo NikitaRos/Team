6b958ae53ab7f8c1dbaa0f8675621a17737dbbcf
Nikita <p.nikita61@mail.ru>
2020-09-19 12:33:22 +0300

    Готовый шар

--- NetProgTCPIP/ClientWinForm/Form1.Designer.cs
+++ NetProgTCPIP/ClientWinForm/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxPred = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxPred
+            // 
+            this.textBoxPred.Location = new System.Drawing.Point(13, 406);
+            this.textBoxPred.Name = "textBoxPred";
+            this.textBoxPred.Size = new System.Drawing.Size(264, 20);
+            this.textBoxPred.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = global::ClientWinForm.Properties.Resources.image_3;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 349);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(294, 450);
+            this.Controls.Add(this.textBoxPred);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxPred;
     }
 }
 
--- NetProgTCPIP/NetProgTCPIP/Program.cs
+++ NetProgTCPIP/NetProgTCPIP/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace NetProgTCPIP
 {
     class Program
     {
+        static readonly TcpListener listerin = new TcpListener(IPAddress.Parse("192.168.88.226"), Convert.ToInt32("1024"));
+        
         static void Main(string[] args)
         {
+            var thread = new Thread(SomeAction);
+            thread.Start();
+            listerin.Start();
+        }
+
+        private static void SomeAction()
+        {
+            string filename = "Predictions.txt";
+            int a;
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+            while (true)
+            {
+                if (listerin.Pending())
+                {
+                    var client = listerin.AcceptTcpClient();
+                    var nstream = client.GetStream();
+
+                    string[] line = File.ReadAllLines(filename);
+                    a = rand.Next(1, line.Length);
+
+                    byte[] barray = Encoding.UTF8.GetBytes(line[a]);
+                    nstream.Write(barray, 0, barray.Length);
+                       
+                    nstream.Close();
+                    client.Close();
+                }
+            }
         }
     }
 }
